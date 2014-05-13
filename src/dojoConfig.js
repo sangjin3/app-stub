71d109a65389a7357a4dea9229fb9d5e6b2f8479
wing754 <wing754@naver.com>
2014-05-13 04:13:12 +0000

    [TASK] Updating webida-lib path [DESC.]

--- src/dojoConfig.js
+++ src/dojoConfig.js
@@ -7,10 +7,10 @@ var dojoConfig = {
     baseUrl: '.',
     parseOnLoad: false,
     packages: [
-        { name: 'webida-lib', location: '//library.' + webidaHost + '/webida/src' },
-        //{ name: 'webida-lib', location: 'library.git/webida/src' },
-        { name: 'all-lib', location: '//library.' + webidaHost },
-        //{ name: 'all-lib', location: 'library.git' },
+        { name: 'webida-lib', location: '//library.' + webidaHost + '/src/webida' },
+        //{ name: 'webida-lib', location: 'library.git/src/webida' },
+        { name: 'all-lib', location: '//library.' + webidaHost + '/src' },
+        //{ name: 'all-lib', location: 'library.git/src' },
     ],
     locale: location.search.match(/locale=([\w\-]+)/) ? RegExp.$1 : "en-us",
 };
